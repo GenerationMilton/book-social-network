23cfb5b51579460e115026decde50eabc528d2a3
Milton Muñoz <livemilton@gmail.com>
2024-05-25 22:22:04 -0500

    primer commit

---
+++ book-network/src/test/java/com/livemilton/book/BookNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livemilton.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
