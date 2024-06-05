14b6d5ffa207ba3862f0c15a9d89744df5610573
Milton Muñoz <livemilton@gmail.com>
2024-06-05 06:55:28 -0500

    exception handling

--- book-network/src/main/java/com/livemilton/book/auth/AuthenticationService.java
+++ book-network/src/main/java/com/livemilton/book/auth/AuthenticationService.java
@@ -109,7 +109,7 @@ public class AuthenticationService {
         return AuthenticationResponse.builder()
                 .token(jwtToken).build();
     }
-    @Transactional
+    //@Transactional
     public void activateAccount(String token) throws MessagingException {
 
         Token savedToken= tokenRepository.findByToken(token)
